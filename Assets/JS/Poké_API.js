c310fbc53076d19677e0607eb7ee08e605b91687
Dmitry Nemov <shikijadenaru@gmail.com>
2020-02-04 14:22:37 +0300

    Added some fix

--- Assets/JS/Poké_API.js
+++ Assets/JS/Poké_API.js
@@ -1,29 +1,56 @@
-const api = 'https://pokeapi.co/api/v2/pokemon?limit=151';
-var table = document.querySelector('.poke_body');
-async function connect(){
-    let request = await fetch(api)
-    let pokemons = await request.json()
-    pokemons.results.forEach(pokemon => {
-        pokemon_detail(pokemon)
-    });
-    async function pokemon_detail(pokemon){
-        let pokemon_url = pokemon.url;
-        let detail_request = await fetch(pokemon_url);
-        let detail = await detail_request.json()
-        draw_table(detail)
-    }
-    function draw_table(detail){
-        // console.log(detail);
-        let pokemon_id = detail.id;
-        let pokemon_weigth = detail.weight
-        // stats.base_stat
-        // stats.stat.name
-        let pokemon_sprite = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + pokemon_id + '.png';
-        let pokemon_name = detail.name;
-        table.innerHTML += '<tr>' + '<td>'+pokemon_id+'</td>' + 
-        '<th>' + '<img src='+pokemon_sprite+'>' + '</th>' + 
-        '<th>' + pokemon_name + '</th>' +'</tr>'
-    }
+const api = "https://pokeapi.co/api/v2/pokemon?limit=151";
+var table = document.querySelector(".poke_body");
+async function connect() {
+  let request = await fetch(api);
+  let pokemons = await request.json();
+  pokemons.results.forEach(pokemon => {
+    pokemon_detail(pokemon);
+  });
+  async function pokemon_detail(pokemon) {
+    let pokemon_url = pokemon.url;
+    let detail_request = await fetch(pokemon_url);
+    let detail = await detail_request.json();
+    draw_table(detail);
+  }
+  function draw_table(detail) {
+    let pokemon = {
+      id: detail.id,
+      name: detail.name,
+      img:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+        detail.id +
+        ".png",
+      weight: detail.weight,
+      height: detail.height
+    };
+    table.innerHTML +=
+      "<tr>" +
+      "<td>" +
+      " -> " +
+      pokemon.id +
+      "</td>" +
+      "<th>" +
+      pokemon.name +
+      "</th>" +
+      "<th>" +
+      "<img src=" +
+      pokemon.img +
+      ">" +
+      "</th>" +
+      "<th>" +
+      "</th>" +
+      "<th>" +
+      "</th>" +
+      "<th>" +
+      pokemon.weight +
+      "</th>" +
+      "<th>" +
+      pokemon.height +
+      "</th>" +
+      "<th>" +
+      "</th>" +
+      "</tr>";
+  }
 }
 
 connect();
