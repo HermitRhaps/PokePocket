d901c0136bdbe47532e9553e8e6250ac22a8ed06
Dmitry Nemov <shikijadenaru@gmail.com>
2020-02-05 15:01:10 +0300

    Added huge fix and more information

--- Assets/JS/Poké_API.js
+++ Assets/JS/Poké_API.js
@@ -10,47 +10,64 @@ async function connect() {
     let pokemon_url = pokemon.url;
     let detail_request = await fetch(pokemon_url);
     let detail = await detail_request.json();
-    draw_table(detail);
-  }
-  function draw_table(detail) {
-    let pokemon = {
-      id: detail.id,
-      name: detail.name,
-      img:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
-        detail.id +
-        ".png",
-      weight: detail.weight,
-      height: detail.height
-    };
-    table.innerHTML +=
-      "<tr>" +
-      "<td>" +
-      " -> " +
-      pokemon.id +
-      "</td>" +
-      "<th>" +
-      pokemon.name +
-      "</th>" +
-      "<th>" +
-      "<img src=" +
-      pokemon.img +
-      ">" +
-      "</th>" +
-      "<th>" +
-      "</th>" +
-      "<th>" +
-      "</th>" +
-      "<th>" +
-      pokemon.weight +
-      "</th>" +
-      "<th>" +
-      pokemon.height +
-      "</th>" +
-      "<th>" +
-      "</th>" +
-      "</tr>";
+    draw_table(detail, detail.types, detail.abilities, detail.stats);
   }
 }
 
+function draw_table(detail, types, abilities, stats) {
+  let type_arr = [];
+  types.forEach(item => {
+    type_arr.push(item.type.name);
+  });
+
+  let ability_arr = [];
+  abilities.forEach(item => {
+    ability_arr.push(item.ability.name);
+  });
+
+  let stat_arr = [];
+  stats.forEach(item => {
+    stat_arr.push(item.base_stat + " " + item.stat.name);
+  });
+
+  let pokemon = {
+    id: detail.id,
+    name: detail.name,
+    img:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+      detail.id +
+      ".png",
+    weight: detail.weight,
+    height: detail.height
+  };
+  table.innerHTML +=
+    "<tr>" +
+    "<td>" +
+    pokemon.id +
+    "</td>" +
+    "<td>" +
+    pokemon.name +
+    "</td>" +
+    "<td>" +
+    '<img src="' +
+    pokemon.img +
+    '"/>' +
+    "</td>" +
+    "<td>" +
+    ability_arr +
+    "</td>" +
+    "<td>" +
+    stat_arr +
+    "</td>" +
+    "<td>" +
+    pokemon.weight +
+    "</td>" +
+    "<td>" +
+    pokemon.height +
+    "</td>" +
+    "<td>" +
+    type_arr +
+    "</td>" +
+    "</tr>";
+}
 connect();
